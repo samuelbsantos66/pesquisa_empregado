8d391b7e1b78d20d48cf1b2463b08512e56d46dc
Samuel Barbosa Santos <125471851+samuelbsantos66@users.noreply.github.com>
2024-11-14 08:52:57 -0300

    Add files via upload

---
+++ Sistema_Empregado/Empregado.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sistema_Empregado
+{
+    internal class Empregado
+    {
+        public int matricula { get; set; }
+        public string cpf { get; set; }
+
+        public string nome { get; set; }
+
+        public string endereco { get; set; }
+
+        public Empregado(string Cpf, string Nome, string Endereco)
+        {
+            cpf = Cpf;
+            nome = Nome;
+            endereco = Endereco;
+        }
+
+        public override string ToString()
+        {
+            return $"Empregado -> Matricula: {matricula} | Cpf: {cpf} | Nome: {nome} | Endereco: {endereco}  ";
+        }
+
+
+        private static void Salvar(Empregado empregado, SqlConnection conexao)
+        {
+            Console.WriteLine(" == Salvando o empregado == ");
+
+            var Cmd = conexao.CreateCommand();
+            Cmd.CommandText = "INSERT INTO EMPREGADO (nome) VALUES (@nome)";
+
+            var param = new SqlParameter("nome", empregado.nome);
+            Cmd.Parameters.Add(param);
+
+            Cmd.ExecuteNonQuery();
+        }
+
+    } 
+}
---
+++ Sistema_Empregado/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sistema_Empregado
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Pesquisar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Salvar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Excluir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Limpar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
